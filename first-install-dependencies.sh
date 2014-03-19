0237493641d895b9c75f4a396c919269383d88e2
Harianto van Insulinde <hariantoatwork@gmail.com>
2014-03-19 01:05:22 +0100

    Fix path

--- first-install-dependencies.sh
+++ first-install-dependencies.sh
@@ -5,6 +5,6 @@ bower install
 cd $ROOT_DIR/laravel
 composer update
 php artisan key:generate
-cd $ROOT_DIR/larave/public/assets/vendor/knockoutjs
+cd $ROOT_DIR/laravel/public/assets/vendor/knockoutjs
 grunt clean checktrailingspaces build:debug test:phantomjs
 cd $ROOT_DIR
